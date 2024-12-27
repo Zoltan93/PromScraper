8d9df764d23c6b272326e993b7408010dc398957
Zoltan93 <zoltan1993@gmail.com>
2024-12-27 08:35:29 +0100

    Add quarkus main app

--- src/main/java/com/zhorvat/quarkus/Application.java
+++ src/main/java/com/zhorvat/quarkus/Application.java
@@ -1,17 +1,12 @@
-//package com.zhorvat.quarkus;
-//
-//import io.quarkus.runtime.annotations.QuarkusMain;
-//
-//import java.util.concurrent.ExecutorService;
-//import java.util.concurrent.Executors;
-//
-//@QuarkusMain
-//public class Application {
-//
-//    private ExecutorService executorService = Executors.newFixedThreadPool(2);
-//
-//    public static void main(String[] args) {
-//
-//
-//    }
-//}
+package com.zhorvat.quarkus;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Application {
+
+    public static void main(String[] args) {
+        Quarkus.run(args);
+    }
+}
