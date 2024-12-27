7d3a42e02666b5fcb713fe3b1489a18e780b14aa
Zoltan93 <zoltan1993@gmail.com>
2024-12-27 12:16:33 +0100

    Add tests for jobmanager

---
+++ src/test/java/com/zhorvat/quarkus/prometheus/JobManagerTests.java
@@ -0,0 +1,31 @@
+package com.zhorvat.quarkus.prometheus;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+@QuarkusTest
+class JobManagerTests {
+
+    @Test
+    void givenSetOfPorts_whenCreatingJobTemplate_thenPortsAreIncluded() {
+        Set<String> ports = Set.of("1234", "5678");
+        String jobTemplate = JobManager.jobTemplate(ports);
+
+        ports.forEach(port ->
+                assertThat(jobTemplate, containsString(port))
+        );
+    }
+
+    @Test
+    void givenEmptySetPorts_whenCreatingJobTemplate_thenPortsAreNotIncluded() {
+        String jobTemplate = JobManager.jobTemplate(Set.of());
+
+        assertThat(jobTemplate, not(containsString("host.docker.internal")));
+    }
+}
