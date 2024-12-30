dfc0ce28ca0a0efb1b92d77a507bf9beb5b43066
Zoltan93 <zoltan1993@gmail.com>
2024-12-30 12:21:36 +0100

    Add test for when container is stopped

--- src/test/java/com/zhorvat/quarkus/docker/ContainerWatcherTests.java
+++ src/test/java/com/zhorvat/quarkus/docker/ContainerWatcherTests.java
@@ -5,8 +5,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
 import com.zhorvat.quarkus.file.YamlManager;
-import com.zhorvat.quarkus.prometheus.JobMapper;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import org.awaitility.Awaitility;
@@ -14,40 +15,113 @@ import org.hamcrest.Matcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @QuarkusTest
 class ContainerWatcherTests {
 
     private final YamlManager yamlManager;
-    private final Client dockerClient;
-    private final JobMapper jobMapper;
+    private final Client client;
     private final ContainerWatcher containerWatcher;
 
     @Inject
     public ContainerWatcherTests(
             YamlManager yamlManager,
-            Client dockerClient,
-            JobMapper jobMapper,
+            Client client,
             ContainerWatcher containerWatcher
     ) {
         this.yamlManager = yamlManager;
-        this.dockerClient = dockerClient;
-        this.jobMapper = jobMapper;
+        this.client = client;
         this.containerWatcher = containerWatcher;
     }
 
     @BeforeEach
     void clearPrometheusTargets() {
         yamlManager.writeToPrometheusYaml(jobTemplate(Collections.emptySet()));
+
+        DockerClient dockerClient = client.buildDockerClient(client.buildDockerClientConfig());
+        Optional<Container> isContainerInExitedState = dockerClient
+                .listContainersCmd()
+                .withShowAll(true)
+                .exec()
+                .stream()
+                .filter(container ->
+                        "exited".equalsIgnoreCase(container.getState())
+                )
+                .findFirst();
+        if (isContainerInExitedState.isPresent()) {
+            startContainer(dockerClient, isContainerInExitedState.get());
+            Awaitility.with()
+                    .pollDelay(4, TimeUnit.SECONDS)
+                    .atMost(20, TimeUnit.SECONDS)
+                    .await()
+                    .until(() -> client.getRunningContainers(dockerClient).size() == 2);
+        }
     }
 
     @Test
-    void given_when_then() {
+    void givenEmptyPrometheusTargetList_whenContainerWatcherIsCalled_thenAdequateTargetIsPopulated() {
         waitForFileCondition(not(containsString("9091")));
         containerWatcher.watch();
         waitForFileCondition(containsString("9091"));
+    }
+
+    @Test
+    void givenNonEmptyPrometheusTargetList_whenContainerIsStoppedAndContainerWatcherIsCalled_thenAdequateTargetIsPopulated() {
+        waitForFileCondition(not(containsString("9091")));
+        containerWatcher.watch();
+        waitForFileCondition(containsString("9091"));
+
+        DockerClient dockerClient = client.buildDockerClient(client.buildDockerClientConfig());
+
+        Set<Container> runningContainers = client.getRunningContainers(dockerClient);
+        stopContainer(dockerClient, runningContainers, "/rabbitmq");
+        Awaitility.with()
+                .pollDelay(4, TimeUnit.SECONDS)
+                .atMost(20, TimeUnit.SECONDS)
+                .await()
+                .until(() -> client.getRunningContainers(dockerClient).size() == 1);
+        containerWatcher.watch();
+        waitForFileCondition(not(containsString("9091")));
+    }
+
+    public void stopContainer(
+            DockerClient dockerClient,
+            Set<Container> containers,
+            String containerToStop
+    ) {
+        containers.forEach(container ->
+                Arrays.stream(container.getNames())
+                        .filter(containerToStop::equalsIgnoreCase)
+                        .findFirst()
+                        .ifPresent(operation ->
+                                dockerClient.stopContainerCmd(container.getId()).exec()
+                        )
+        );
+    }
+
+    public void startContainer(
+            DockerClient dockerClient,
+            Container containerToStart
+    ) {
+        dockerClient.startContainerCmd(containerToStart.getId()).exec();
+    }
+
+    public Optional<String> isContainerRunning(
+            Set<Container> containers,
+            String containerToStart
+    ) {
+        return containers.stream().map(container ->
+                        Arrays.stream(container.getNames())
+                                .filter(containerToStart::equalsIgnoreCase)
+                                .findFirst()
+                )
+                .flatMap(Optional::stream)
+                .findFirst();
     }
 
     private void waitForFileCondition(Matcher<String> condition) {
